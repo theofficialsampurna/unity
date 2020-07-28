ca056aadba1f6d18117324c42dce4e2062040b13
theofficialsampurna <theofficialsampurnashrestha@gmail.com>
2020-07-28 19:22:26 +0545

    added unity projects

---
+++ Bald Ninja/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    float horizontal;
+    public float speed = 3.0f;
+
+    // Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        // rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+      
+        horizontal = Input.GetAxis("Horizontal");
+        Vector2 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+
+        transform.position = position;
+        
+    }
+
+}

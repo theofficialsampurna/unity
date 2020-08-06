dca41ff9726cd5230ab86dd7ede8ffeecb6a39a1
theofficialsampurna <theofficialsampurnashrestha@gmail.com>
2020-08-06 14:22:04 +0545

    added movement for player and animation

---
+++ Ninja 2D/Assets/Scripts/Ninja.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour
+{
+    public float moveSpeed = 40f;
+    float horizontalMove = 0f;
+
+    private Rigidbody2D rbNinja;
+    public Animator animator;
+
+    private void Awake()
+    {
+        rbNinja = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(horizontalMove * Time.fixedDeltaTime);
+    }
+
+    private void MovePlayer(float move)
+    {
+        rbNinja.velocity = new Vector2(move, 0);
+    }
+}
